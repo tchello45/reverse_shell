ed9c2c7899abb2767181f549cfd7fc8a42a553db
Tilman Kurmayer <tilman.kurmayer@gmail.com>
2025-08-27 16:51:46 +0200

    feat: :sparkles: Implement socket creation and message sending in ASM client

--- reverse_shell.asm
+++ reverse_shell.asm
@@ -1,7 +1,42 @@
+section .data
+    address:
+        dw 2
+        dw 0x901F
+        dd 0x0100007F
+        dq 0
+
+    address_len equ $ - address
+
+    msg db "Hello from the ASM client!", 0xa
+    msg_len equ $ - msg
+
+section .bss
+    socket_fd resq 1
+
 section .text
     global _start
 
 _start:
+    mov rax, 41
+    mov rdi, 2
+    mov rsi, 1
+    mov rdx, 0
+    syscall
+
+    mov [socket_fd], rax
+
+    mov rax, 42
+    mov rdi, [socket_fd]
+    mov rsi, address
+    mov rdx, address_len
+    syscall
+
+    mov rax, 1
+    mov rdi, [socket_fd]
+    mov rsi, msg
+    mov rdx, msg_len
+    syscall
+
     mov rax, 60
     mov rdi, 0
     syscall
